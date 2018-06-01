6af4e93aac3002d16d202a79b8da4def971e937a
Daniel <Shamaran85@gmail.com>
2018-06-01 13:17:31 +0200

    Added Upload component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { BackComponent } from './back/back.component';
 import { LogoutComponent } from './logout/logout.component';
 import { DatalistComponent } from './datalist/datalist.component';
 import { LoginComponent } from './login/login.component';
+import { UploadComponent } from './upload/upload.component';
 
 // Breadcrumbs
 // Upload
@@ -29,7 +30,8 @@ import { LoginComponent } from './login/login.component';
     MainComponent,
     BackComponent,
     LogoutComponent,
-    LoginComponent
+    LoginComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule,
