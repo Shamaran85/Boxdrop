b4a0722a806016cd998b8aba76a65fe356f962a3
Daniel <Shamaran85@gmail.com>
2018-06-04 21:36:07 +0200

    removed old import file

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Dropbox } from 'dropbox';
-import { DropboxService } from '../dropbox.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { DataService } from '../data.service';
 
@@ -11,7 +10,7 @@ import { DataService } from '../data.service';
 })
 export class UploadComponent implements OnInit {
 
-  constructor(public dropbox: DataService, private router: Router, private activeroute: ActivatedRoute) {}
+  constructor(public dropbox: DataService, private router: Router, private activeroute: ActivatedRoute) { }
 
   ngOnInit() {
   }
@@ -19,24 +18,24 @@ export class UploadComponent implements OnInit {
   uploadYourFile() {
     // alert('Upload!!!');
     //console.log(this.router.url);
-var ACCESS_TOKEN = 'PjVheRC4lNAAAAAAAAAAEeQoR7oz26-cQRI-5zpne2ZARERKVEjHU3f_Vq932ccC';
-var dbx = new Dropbox({ accessToken: ACCESS_TOKEN});
-var fileInput = (<HTMLInputElement> document.getElementById('file-upload'));
-var file = fileInput.files[0];
-console.log(file, ACCESS_TOKEN);
-const filePath = this.router.url + '/' + file.name;
-console.log(filePath);
-dbx.filesUpload({path: filePath, contents: file})
-  .then(function(response) {
-    //var results = document.getElementById('results');
-    //results.appendChild(document.createTextNode('File uploaded!'));
-    console.log(response);
-    location.reload();
-  })
-  .catch(function(error) {
-    console.error(error);
-  });
-return false;
+    var ACCESS_TOKEN = 'PjVheRC4lNAAAAAAAAAAEeQoR7oz26-cQRI-5zpne2ZARERKVEjHU3f_Vq932ccC';
+    var dbx = new Dropbox({ accessToken: ACCESS_TOKEN });
+    var fileInput = (<HTMLInputElement>document.getElementById('file-upload'));
+    var file = fileInput.files[0];
+    console.log(file, ACCESS_TOKEN);
+    const filePath = this.router.url + '/' + file.name;
+    console.log(filePath);
+    dbx.filesUpload({ path: filePath, contents: file })
+      .then(function (response) {
+        //var results = document.getElementById('results');
+        //results.appendChild(document.createTextNode('File uploaded!'));
+        console.log(response);
+        location.reload();
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+    return false;
   }
 
 
