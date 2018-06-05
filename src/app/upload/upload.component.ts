1126bcd71e8556892b2b434ec3360966d898233e
Daniel <Shamaran85@gmail.com>
2018-06-05 10:29:31 +0200

    Fixed upload on root directory

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -15,20 +15,14 @@ export class UploadComponent implements OnInit {
   ngOnInit() {
   }
 
-  uploadYourFile() {
-    // alert('Upload!!!');
-    //console.log(this.router.url);
-    var ACCESS_TOKEN = 'PjVheRC4lNAAAAAAAAAAEeQoR7oz26-cQRI-5zpne2ZARERKVEjHU3f_Vq932ccC';
-    var dbx = new Dropbox({ accessToken: ACCESS_TOKEN });
-    var fileInput = (<HTMLInputElement>document.getElementById('file-upload'));
-    var file = fileInput.files[0];
-    console.log(file, ACCESS_TOKEN);
-    const filePath = this.router.url + '/' + file.name;
-    console.log(filePath);
+  uploadFile() {
+    const accessToken = this.dropbox.accessToken;
+    const dbx = new Dropbox({ accessToken: accessToken });
+    const fileInput = (<HTMLInputElement>document.getElementById('file-upload'));
+    const file = fileInput.files[0];
+    const filePath = this.router.url !== '' ? this.router.url + file.name : this.router.url + '/' + file.name;
     dbx.filesUpload({ path: filePath, contents: file })
       .then(function (response) {
-        //var results = document.getElementById('results');
-        //results.appendChild(document.createTextNode('File uploaded!'));
         console.log(response);
         location.reload();
       })
