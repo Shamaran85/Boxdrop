9f40b5bbf54566f3a431de56e9bfbdb3d9aac09b
Daniel <Shamaran85@gmail.com>
2018-06-11 19:53:51 +0200

    Refactored firebase function

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -3,28 +3,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
-// import { AngularFirestore, AngularFirestoreDocument } from '../../node_modules/angularfire2/firestore';
-// admin.initializeApp(functions.config().firebase);
-// admin.initializeApp(functions.config().firebase);
-// const db = admin.firestore();
 const db = admin.database();
 exports.webhook = functions.https.onRequest((request, response) => {
-    // skriv i databasen update timestamp etc
-    test();
-    response.send(request.query.challenge);
-});
-function test() {
-    console.log('hej');
     const newDate = new Date();
     const displayDate = newDate.toISOString().slice(0, 19).replace('T', ' ');
     db.ref('updates/').set({
         updated: displayDate
     })
         .then(function () {
-        console.log("New Updates at: ", displayDate);
+        console.log("New Update: ", displayDate);
     })
         .catch(function (error) {
-        console.error("Error updating: ", error);
+        console.error("Error: ", error);
     });
-}
+    response.send(request.query.challenge);
+});
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,22 +2,9 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 admin.initializeApp();
 
-// import { AngularFirestore, AngularFirestoreDocument } from '../../node_modules/angularfire2/firestore';
-// admin.initializeApp(functions.config().firebase);
-// admin.initializeApp(functions.config().firebase);
-
-// const db = admin.firestore();
-
 const db = admin.database();
 
 export const webhook = functions.https.onRequest((request, response) => {
-    // skriv i databasen update timestamp etc
-    test();
-    response.send(request.query.challenge);
-});
-
-function test() {
-    console.log('hej');
 
     const newDate = new Date();
     const displayDate = newDate.toISOString().slice(0, 19).replace('T', ' ');
@@ -25,10 +12,12 @@ function test() {
         updated: displayDate
     })
         .then(function () {
-            console.log("New Updates at: ", displayDate);
+            console.log("New Update: ", displayDate);
         })
         .catch(function (error) {
-            console.error("Error updating: ", error);
+            console.error("Error: ", error);
         });
 
-}
+
+    response.send(request.query.challenge);
+});
